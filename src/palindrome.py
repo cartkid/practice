96f78954a6936bee79682702cb289de94ca486f0
cartkid <ericlevicky@gmail.com>
2023-05-16 19:46:59 -0400

    add other ways of checking

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,3 +1,24 @@
 def is_palindrome(input: str) -> bool:
     test = input[::-1]
     return test == input
+
+
+def is_palindrome2(input: str) -> bool:
+    up_to_index: int = int(len(input) / 2)
+    if len(input) % 2 == 0:
+        up_to_index -= 1
+    print(f"{len(input)}: {up_to_index}")
+    test = input[:up_to_index:-1]
+    print(f"{input}: {test}")
+    return input.startswith(test)
+
+
+def is_palindrome3(input: str) -> bool:
+    up_to_index: int = int(len(input) / 2)
+    print(f"{len(input)}: {up_to_index}")
+    for i in range(up_to_index):
+        if input[i] == input[len(input) - 1 - i]:
+            continue
+        else:
+            return False
+    return True
